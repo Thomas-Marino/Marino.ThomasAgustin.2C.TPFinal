44588b86a620b4b67699ddce9a80ceedad349d22
Thomas-Marino <thomasmarino627@gmail.com>
2023-11-17 15:13:25 -0300

    Implementación de interfaces

---
+++ Entidades/Interfaces/IValidacionesPersona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Entidades.Interfaces
+{
+    public interface IValidacionesPersona
+    {
+        string ValidacionNombre(string nombre);
+        string ValidacionApellido(string apellido);
+        int ValidacionDni(string dni);
+    }
+}
---
+++ Entidades/Interfaces/IValidacionesUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Interfaces
+{
+    internal interface IValidacionesUsuario
+    {
+        string ValidacionContraseña(string contraseña);
+        string ValidacionUsuario(string usuario);
+    }
+}
